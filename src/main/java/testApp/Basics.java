325f6ac170f176a49e010d25ede0964b25d571a2
Crandel <cradlemann@gmail.com>
2017-06-27 08:49:53 +0300

    Add stdin

--- src/main/java/testApp/Basics.java
+++ src/main/java/testApp/Basics.java
@@ -1,6 +1,15 @@
 package testApp;
 
+import java.util.Scanner;
+
 public class Basics {
+  static int x;
+  static String name;
+  static String username;
+
+  static {
+    x = 5;
+  }
 
   public Basics() {
     // Fill second constructor with Default value
@@ -8,21 +17,30 @@ public class Basics {
     System.out.println("Constructor without args");
   }
 
-  public Basics(final String name) {
+  public Basics(String classname) {
+    name = classname;
     System.out.format("One arg constructor with name: %s\n", name);
   }
 
   public static void run() {
+    input();
     vars();
   }
 
   public static void vars() {
-    int x = 5;
+    System.out.println(x);
     int y = 6;
     System.out.println(6/x + 5*y);
     int z = x;
     System.out.println(z);
     z = 13;
     System.out.format("x is %d and z is %d\n", x, z);
+    System.out.format("Class %s from user %s\n", name, username);
+  }
+
+  public static void input() {
+    System.out.println("Please enter your name");
+    Scanner scan = new Scanner(System.in);
+    username = scan.nextLine();
   }
 }
