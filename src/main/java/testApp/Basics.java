1c8a30a6579ee13ad305351e89383b385107a83a
Crandel <cradlemann@gmail.com>
2017-07-02 12:22:45 +0300

    Input test

--- src/main/java/testApp/Basics.java
+++ src/main/java/testApp/Basics.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Basics {
   static int x = 3;
+  private int ux = 0;
+  private double ud = 0.0;
   private String name = "Def name";
   private String username = "";
   // static means this number is shared by all objects of type Animal
@@ -21,13 +23,13 @@ public class Basics {
   public Basics() {
     // Fill second constructor with Default value
     this("Default name");
-    this.name = "Default name";
+    this.setUsername("Default username");
     System.out.println("Constructor without args");
   }
 
-  public Basics(String classname) {
-    this.name = classname;
-    System.out.format("One arg constructor with name: %s\n", name);
+  public Basics(String name) {
+    this.setName(name);
+    System.out.format("One arg constructor with name: %s\n", getName());
   }
 
   public String getUsername() {
@@ -36,6 +38,30 @@ public class Basics {
 
   public void setUsername(String username) {
     this.username = username;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getUx() {
+    return ux;
+  }
+
+  public void setUx(int ux) {
+    this.ux = ux;
+  }
+
+  public double getUd() {
+    return ud;
+  }
+
+  public void setUd(double ud) {
+    this.ud = ud;
   }
 
   public void run() {
@@ -52,11 +78,18 @@ public class Basics {
     System.out.println(z);
     z = 13;
     System.out.format("x is %d and z is %d\n", x, z);
-    System.out.format("Class %s from user %s\n", this.name, this.username);
+    System.out.format("Class %s from user %s\n", this.getName(), this.getUsername());
   }
 
   private void input() {
-    System.out.println("Please enter your real name");
-    username = userInput.nextLine();
+    System.out.println("Please enter something");
+    if (userInput.hasNextInt()) {
+      setUx(userInput.nextInt());
+    } else if (userInput.hasNextDouble()) {
+      setUd(userInput.nextDouble());
+    } else if (userInput.hasNextLine()) {
+      setUsername(userInput.nextLine());
+    }
+    System.out.format("username is %s, ux is %d, ud is %3.4f\n", getUsername(), getUx(), getUd());
   }
 }
