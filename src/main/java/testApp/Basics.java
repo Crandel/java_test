a44c5e6b4bf5a255e1733fdd30026076ee063bda
Vitalii Drevenchuk <vitalii.drevenchuk@accenture.com>
2017-07-04 14:09:51 +0300

    Add triangle)

--- src/main/java/testApp/Basics.java
+++ src/main/java/testApp/Basics.java
@@ -65,8 +65,9 @@ public class Basics {
   }
 
   public void run() {
-    input();
-    vars();
+    // input();
+    // vars();
+    triangle();
   }
 
   private void vars() {
@@ -95,5 +96,22 @@ public class Basics {
       setUsername(userInput.nextLine());
     }
     System.out.format("username is %s, ux is %d, ud is %3.4f\n", getUsername(), getUx(), getUd());
+  }
+
+  private void triangle() {
+    int a = 8;
+    int l = 10;
+    int p = l;
+    for (int i = 0; i < l; i++) {
+      for (int j = 0; j < p; j++ ){
+       System.out.print(" ");
+      }
+      p--;
+      System.out.print(a);
+      for (int j = 0; j < i; j++) {
+        System.out.print(a);
+      }
+      System.out.println();
+    }
   }
 }
